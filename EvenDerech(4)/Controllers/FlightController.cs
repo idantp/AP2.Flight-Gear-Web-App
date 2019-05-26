5508cb0a79bb2b3e68fea687d0ebf1c9a6bc84b7
Roy Hevrony <roy.hevrony@live.biu.ac.il>
2019-05-26 17:16:37 +0300

    views

---
+++ EvenDerech(4)/Controllers/FlightController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EvenDerech_4_.Controllers
+{
+    public class FlightController : Controller
+    {
+        // GET: Flight
+        public ActionResult LocatePlane()
+        {
+            return View();
+        }
+
+        public ActionResult FlightPath()
+        {
+            return View();
+        }
+
+        public ActionResult SaveFlightData()
+        {
+            return View();
+        }
+
+        public ActionResult LoadFlightData()
+        {
+            return View();
+        }
+    }
+}
