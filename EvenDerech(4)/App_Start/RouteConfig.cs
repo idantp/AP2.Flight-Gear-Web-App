8b29d977d1d2ed4ce1bcd97f7f6014a6b76872a4
idant <idant@LAPTOP-1CBL0P6J>
2019-05-26 17:36:17 +0300

    views

--- EvenDerech(4)/App_Start/RouteConfig.cs
+++ EvenDerech(4)/App_Start/RouteConfig.cs
@@ -11,12 +11,12 @@ namespace EvenDerech_4_
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}")f;
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Flight", action = "LocatePlane", id = UrlParameter.Optional }
             );
         }
     }
